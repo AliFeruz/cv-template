fee9f10e90d66550e8e8a191e46fd01bc43c88e0
Ali Feruz <147985313+AliFeruz@users.noreply.github.com>
2024-01-21 21:38:49 +0100

    Done

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -7,24 +7,24 @@ import {
 
 const Header = () => {
   return (
-    <div className="flex-col w-full p-8">
-      <div className="text-center mt-2">
-        <h1 className="text-xl text-center underline underline-offset-8 font-semibold">
+    <div className="flex-col w-full px-4 py-10">
+      <div className="text-center mt-4">
+        <h1 className="text-2xl text-center underline underline-offset-8 font-semibold">
           CONTACT INFO
         </h1>
       </div>
-      <div className="flex-col mt-4 bg-teal-100 border border-teal-500 rounded-xl p-4 w-full">
+      <div className="flex-col mt-4 bg-teal-100 border border-orange-500 rounded-xl p-4">
         <div className="flex items-center w-auto mt-2">
           <HomeIcon className="w-6 h-6" />
-          <p className="text-xl ml-4 px-3 text-teal-700 font-semibold">Berlin</p>
+          <p className="text-xl ml-4 px-3 text-orange-400 font-semibold">Berlin</p>
         </div>
         <div className="flex items-center mt-2">
           <PhoneIcon className="w-6 h-6" />
-          <p className="text-xl ml-4 px-3 text-teal-700 font-semibold">+49 162 921</p>
+          <p className="text-xl ml-4 px-3 text-orange-400 font-semibold">+49 162 921 6040</p>
         </div>
         <div className="flex items-center mt-2">
           <EnvelopeIcon className="w-6 h-6" />
-          <p className="text-xl ml-4 px-3 text-teal-700 font-semibold">
+          <p className="text-xl ml-4 px-3 text-orange-400 font-semibold">
             aliferuzcode@gmail.com
           </p>
         </div>
@@ -34,7 +34,7 @@ const Header = () => {
             className="flex mt-2 items-center"
           >
             <GlobeAltIcon className="w-6 h-6" />
-            <p className="text-xl ml-4 px-3 text-teal-700 font-semibold">Ali Feruz</p>
+            <p className="text-xl ml-4 px-3 text-orange-400 font-semibold">Ali Feruz</p>
           </a>
           <a
             href="https://www.linkedin.com/in/ali-feruz-5840ab164/"
@@ -45,49 +45,49 @@ const Header = () => {
               alt="linkedin"
               className="w-6 h-6"
             />
-            <p className="text-xl ml-4 px-3 text-teal-700 font-semibold">LinkedIn</p>
+            <p className="text-xl ml-4 px-3 text-orange-400 font-semibold">LinkedIn</p>
           </a>
           <a
             href=" https://github.com/AliFeruz"
             className="flex items-center mt-2"
           >
             <img src="/assets/github.png" alt="linkedin" className="w-6 h-6" />
-            <p className="text-xl ml-4 px-3 text-teal-700 font-semibold">Github</p>
+            <p className="text-xl ml-4 px-3 text-orange-400 font-semibold">Github</p>
           </a>
         </div>
       </div>
-      <div className="text-center mt-4">
-        <h1 className="text-xl text-center underline underline-offset-8 font-semibold">
+      <div className="text-center mt-6">
+        <h1 className="text-2xl text-center underline underline-offset-8 font-semibold">
           EDUCATION
         </h1>
       </div>
-      <div className="flex-col mt-4 bg-teal-100 border border-teal-500 rounded-xl p-4 w-full">
-        <h2 className="text-xl text-teal-800 font-semibold">
+      <div className="flex-col mt-4 bg-teal-100 border border-orange-500 rounded-xl p-4 w-full">
+        <h2 className="text-xl text-center text-orange-400 font-semibold">
           CODE ACADEMY BERLIN (Bootcamp)
         </h2>
-        <p className="p-1 mt-2">09.2023 - 03.2024</p>
+        <p className="p-1 mt-2 text-center">09.2023 - 03.2024</p>
       </div>
-      <div className="text-center mt-4">
-        <h1 className="text-xl text-center underline underline-offset-8 font-semibold">
+      <div className="text-center mt-6">
+        <h1 className="text-2xl text-center underline underline-offset-8 font-semibold">
           SKILL'S
         </h1>
       </div>
-      <div className="grid grid-cols-2 gap-4  p-4 w-full">
-        <div className="flex bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2">
+      <div className="grid grid-cols-2 gap-4 mt-2  py-4 w-full">
+        <div className="flex bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2">
           <img src="/assets/bxl-html5.svg" alt="html5" className="w-10 h-10" />
           <div className="flex-col">
             <h1 className="text-xl px-2 text-teal-800 font-semibold">HTML5</h1>
             <p className="px-2 font-semibold">Basic</p>
           </div>
         </div>
-        <div className="flex bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2">
+        <div className="flex bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2">
           <img src="/assets/bxl-css3.svg" alt="css3" className="w-10 h-10" />
           <div className="flex-col">
             <h1 className="text-xl px-2 text-teal-800 font-semibold">CSS3</h1>
             <p className="px-2 font-semibold">Basic</p>
           </div>
         </div>
-        <div className="flex bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2">
+        <div className="flex bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2">
           <img
             src="/assets/bxl-javascript.svg"
             alt="javascript"
@@ -98,7 +98,7 @@ const Header = () => {
             <p className="px-2 font-semibold">Basic</p>
           </div>
         </div>
-        <div className="flex bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2">
+        <div className="flex bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2">
           <img
             src="/assets/bxl-typescript.svg"
             alt="typescript"
@@ -110,14 +110,14 @@ const Header = () => {
           </div>
         </div>
         
-        <div className="flex bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2">
+        <div className="flex bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2">
           <img src="/assets/bxl-react.svg" alt="react" className="w-10 h-10" />
           <div className="flex-col">
             <h1 className="text-xl px-2 text-teal-800 font-semibold">React</h1>
             <p className="px-2 font-semibold">Basic</p>
           </div>
         </div>
-        <div className="flex bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2">
+        <div className="flex bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2">
           <img
             src="/assets/bxl-nodejs.svg"
             alt="nodejs"
@@ -128,7 +128,7 @@ const Header = () => {
             <p className="px-2 font-semibold">Basic</p>
           </div>
         </div>
-        <div className="flex bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2">
+        <div className="flex bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2">
           <img
             src="/assets/bxl-tailwind-css.svg"
             alt="tailwind-css"
@@ -139,7 +139,7 @@ const Header = () => {
             <p className="px-2 font-semibold">Basic</p>
           </div>
         </div>
-        <div className="flex bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2">
+        <div className="flex bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2">
           <img
             src="/assets/bxl-mongodb.svg"
             alt="mongodb"
@@ -150,28 +150,28 @@ const Header = () => {
             <p className="px-2 font-semibold">Basic</p>
           </div>
         </div>
-        <div className="flex bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2">
+        <div className="flex bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2">
           <img src="/assets/bxl-git.svg" alt="git" className="w-10 h-10" />
           <div className="flex-col">
             <h1 className="text-xl px-2 text-teal-800 font-semibold">Git</h1>
             <p className="px-2 font-semibold">Basic</p>
           </div>
         </div>
-        <div className="flex bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2">
+        <div className="flex bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2">
           <img src="/assets/next-js.svg" alt="git" className="w-10 h-10" />
           <div className="flex-col">
             <h1 className="text-xl px-2 text-teal-800 font-semibold">NextJS</h1>
             <p className="px-2 font-semibold">Basic</p>
           </div>
         </div>
-        <div className="flex bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2">
+        <div className="flex bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2">
           <img src="/assets/graphql-icon.svg" alt="git" className="w-10 h-10" />
           <div className="flex-col">
             <h1 className="text-xl text-teal-800 font-semibold">GraphQL</h1>
             <p className="px-2 font-semibold">Basic</p>
           </div>
         </div>
-        <div className="flex bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2">
+        <div className="flex bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2">
           <img src="/assets/github.png" alt="git" className="w-10 h-10" />
           <div className="flex-col">
             <h1 className="text-xl px-2 text-teal-800 font-semibold">Github</h1>
@@ -179,43 +179,43 @@ const Header = () => {
           </div>
         </div>
       </div>
-      <div className="text-center mt-4">
-        <h1 className="text-xl text-center underline underline-offset-8 font-semibold">
+      <div className="text-center mt-6">
+        <h1 className="text-2xl text-center underline underline-offset-8 font-semibold">
           LANGUAGE SKILL'S
         </h1>
       </div>
-      <div className="grid grid-cols-2 gap-4  p-4 w-full">
-        <div className="flex-col bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2">
+      <div className="grid grid-cols-2 gap-4  mt-2 py-4 w-full">
+        <div className="flex-col bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2">
           <h1 className="text-2xl ml-2 p-1 text-teal-700 font-semibold">
             English
           </h1>
           <p className="text-xl ml-2 p-1 font-semibold">Proficient</p>
         </div>
-        <div className="flex-col bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2">
+        <div className="flex-col bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2">
           <h1 className="text-2xl ml-2 p-1 text-teal-700 font-semibold">
             Russian
           </h1>
           <p className="text-xl ml-2 p-1 font-semibold">Native</p>
         </div>
-        <div className="flex-col bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2">
+        <div className="flex-col bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2">
           <h1 className="text-2xl ml-2 p-1 text-teal-700 font-semibold">
             German
           </h1>
           <p className="text-xl ml-2 p-1 font-semibold">Beginner</p>
         </div>
-        <div className="flex-col bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2">
+        <div className="flex-col bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2">
           <h1 className="text-2xl ml-2 p-1 text-teal-700 font-semibold">
             Uzbek
           </h1>
           <p className="text-xl ml-2 p-1 font-semibold">Native</p>
         </div>
-        <div className="flex-col bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2">
+        <div className="flex-col bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2">
           <h1 className="text-2xl ml-2 p-1 text-teal-700 font-semibold">
             Arabic
           </h1>
           <p className="text-xl ml-2 p-1 font-semibold">Advanced</p>
         </div>
-        <div className="flex-col bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2">
+        <div className="flex-col bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2">
           <h1 className="text-2xl ml-2 p-1 text-teal-700 font-semibold">
             Persian
           </h1>
--- src/sections/Info.tsx
+++ src/sections/Info.tsx
@@ -1,47 +1,115 @@
-import { GlobeAltIcon } from "@heroicons/react/24/outline"
 
 
 
 const Info = () => {
   return (
-    <div className="flex-col w-full p-8">
+    <div className="flex-col w-full p-4">
+      
         <div className="flex-col flex justify-center items-center w-full p-4 mt-4">
-            <img src="/assets/profile.jpeg" alt="profile" className="w-[300px] h-[330px] shadow shadow-teal-500 object-cover ml-14 rounded-lg" />
-            <h1 className="text-3xl text-teal-500 font-bold mt-6">ALI FERUZ</h1>
-            <h1 className="text-2xl text-teal-600 font-bold mt-2">Full Stack Web Developer</h1>
+            <img src="/assets/profile.jpeg" alt="profile" className="w-[350px] h-[360px] shadow-lg shadow-orange-500 object-cover opacity-80 ml-14 rounded-xl" />
+            <h1 className="text-3xl text-orange-500 font-bold mt-6">ALI FERUZ</h1>
+            <h1 className="text-2xl text-teal-700 font-bold mt-2">Junior Full Stack Web Developer</h1>
         </div>
-        <div className="flex-col mt-4 bg-teal-100 border border-teal-500 rounded-xl p-4 w-full">
+        <div className="flex-col mt-4 bg-teal-100 border border-orange-500 rounded-xl p-4 w-full">
             <p className="text-xl p-2 font-semibold">Enthusiastic Junior Full Stack Developer with a strong foundation in web development technologies. Proficient in HTML, CSS, and JavaScript, with hands-on experience in building responsive and dynamic web applications using React and Node.js. Well-versed in the modern development workflow, utilizing Git/Github for version control.</p>
             <p className="text-xl p-2 font-semibold">Specialized in crafting sleek and intuitive user interfaces with proficiency in styling frameworks like Tailwind CSS. Experienced in database management, with a solid understanding of MongoDB and Firebase, ensuring seamless integration of data-driven functionalities.</p>
             <p className="text-xl p-2 font-semibold">Eager to contribute my technical skills and passion for innovation to a dynamic development team at [Company/Organization]. Excited about the opportunity to apply and expand my expertise in full-stack development to drive impactful solutions and deliver exceptional user experiences.</p>
         </div>
-        <div className="text-center mt-4">
-        <h1 className="text-xl text-center underline underline-offset-8 font-semibold">
+        <div className="text-center mt-6">
+        <h1 className="text-2xl text-center underline underline-offset-8 font-semibold">
           EXPERIENCE & PROJECTS
         </h1>
       </div>
-      <div className="grid grid-cols-2 gap-4 p-4">
-        <div className="flex-col bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-4 mt-2">
-            <h1>Project Name</h1>
-            <p>Description</p>
+      <div className="grid grid-cols-2 gap-4 mt-2 py-4">
+        <div className="flex-col border border-orange-500 rounded-xl p-1 items-center gap-4 mt-2">
+            <h1 className="text-center mt-1 text-xl text-orange-500">RickAndMorty</h1>
+            <p className="p-2">This project made to fix new knowledge about NextJS. <br/> During this project I'm fetching data using REST API and GraphQL. I'm generating my pages SSR, CSR and SSG</p>
             <div className="flex">
-            <a
-            href=" https://github.com/AliFeruz"
-            className="flex-col bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2"
+              <div className="flex justify-between gap-3 w-full p-1 items-center">
+              <a
+            href="https://github.com/AliFeruz/rickandmorty"
+            className="flex-col bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2"
           >
-            <img src="/assets/github.png" alt="linkedin" className="w-6 h-6" />
-            <p className="text-xl ml-4 px-3 text-teal-700 font-semibold">Github</p>
+            <p className=" text-orange-400 font-semibold">Github Repo</p>
           </a>
           <a
-            href="https://ali-feruz.vercel.app/"
-            className="flex-col bg-teal-200 border border-teal-500 rounded-xl p-1 items-center gap-2 mt-2"
+            href="https://rickandmorty-roan.vercel.app/"
+            className="flex-col bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2"
           >
-            <GlobeAltIcon className="w-6 h-6" />
-            <p className="text-xl ml-4 px-3 text-teal-700 font-semibold">Ali Feruz</p>
+            <p className=" text-orange-400 font-semibold">Live Demo</p>
           </a>
+              </div>
+            </div>
+        </div>
+        <div className="flex-col border border-orange-500 rounded-xl p-1 items-center gap-4 mt-2">
+            <h1 className="text-center mt-1 text-xl text-orange-500">CRUD Notes</h1>
+            <p className="p-2">MERN Notes app. Create | Read | Update | Delete <br/>
+            To use CRUD Notes app first you need to create account, then you will get features to create, update and delete Notes.</p>
+            <div className="flex">
+              <div className="flex justify-between gap-3 w-full p-1 items-center">
+              <a
+            href="https://github.com/AliFeruz/mern_notes"
+            className="flex-col bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2"
+          >
+            <p className=" text-orange-400 font-semibold">Github Repo</p>
+          </a>
+          <a
+            href="https://crudnotes.vercel.app/"
+            className="flex-col bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2"
+          >
+            <p className=" text-orange-400 font-semibold">Live Demo</p>
+          </a>
+              </div>
+            </div>
+        </div>
+        <div className="flex-col border border-orange-500 rounded-xl p-1 items-center gap-4 mt-2">
+            <h1 className="text-center mt-1 text-xl text-orange-500">Rakhudu</h1>
+            <p className="p-2"> To build this React app, I used my knowledge of Typescript, Tailwind CSS and more</p>
+            <div className="flex">
+              <div className="flex justify-between gap-3 w-full p-1 items-center">
+              <a
+            href="https://github.com/AliFeruz/Rakhudu"
+            className="flex-col bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2"
+          >
+            <p className=" text-orange-400 font-semibold">Github Repo</p>
+          </a>
+          <a
+            href="https://marggraff.vercel.app/"
+            className="flex-col bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2"
+          >
+            <p className=" text-orange-400 font-semibold">Live Demo</p>
+          </a>
+              </div>
+            </div>
+        </div>
+        <div className="flex-col border border-orange-500 rounded-xl p-1 items-center gap-4 mt-2">
+            <h1 className="text-center mt-1 text-xl text-orange-500">Pokedex</h1>
+            <p className="p-2">This project builded on Vanilla HTML, CSS and JS<br/>
+            Fetching data from PokeApi restapi</p>
+            <div className="flex">
+              <div className="flex justify-between gap-3 w-full p-1 items-center">
+              <a
+            href="https://github.com/AliFeruz/pokedex"
+            className="flex-col bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2"
+          >
+            <p className=" text-orange-400 font-semibold">Github Repo</p>
+          </a>
+          <a
+            href="https://pokedex1gen.vercel.app/index.html"
+            className="flex-col bg-teal-100 border border-orange-500 rounded-xl p-1 items-center gap-2 mt-2"
+          >
+            <p className=" text-orange-400 font-semibold">Live Demo</p>
+          </a>
+              </div>
             </div>
         </div>
       </div>
+      <div className="gap-4 justify-end p-4 flex">
+        <p className="mt-2 text-xl">More projects you can see on my</p>
+        <a href=" https://github.com/AliFeruz" className="flex items-center mt-2">
+        <p className="text-xl ml-2 text-orange-400 font-semibold">Github</p>
+          </a>
+        </div>
         
     </div>
   )
